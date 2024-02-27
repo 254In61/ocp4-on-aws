f93ac819a53acfeaf6e924dea579569d7f8ca9b8
254In61 <allanmaseghe4@gmail.com>
2024-02-27 12:33:26 +1000

    Tue 27 Feb 2024 12:33:26 AEST - Updates

--- scripts/cluster-scheduler.sh
+++ scripts/cluster-scheduler.sh
@@ -2,10 +2,12 @@
 
 # Script to confirm cluster scheduler is put to false.
 
-mastersSchedulable=yq -p yaml -o json $HOME/manifests/cluster-scheduler-02-config.yml | jq -c .spec.mastersSchedulable
+# mastersSchedulable=yq -p yaml -o json $HOME/manifests/cluster-scheduler-02-config.yml | jq -c .spec.mastersSchedulable
+
+output=cat $HOME/manifests/cluster-scheduler-02-config.yml | grep mastersSchedulable
 
 # Check if the mastersSchedulable equals false
-if [ "$mastersSchedulable" != "false" ]; then
+if [ "output" != "mastersSchedulable: false" ]; then
     echo "FAIL"
     exit 1
 fi
