8fcaf8f22c4c8e159a051c189f4275d078f148ff
254In61 <allanmaseghe4@gmail.com>
2024-02-28 13:41:02 +1000

    Wed 28 Feb 2024 13:41:02 AEST - Updates

--- scripts/ec2-env-prep.sh
+++ scripts/ec2-env-prep.sh
@@ -20,7 +20,7 @@ python_boto3(){
 aws_cli(){
    echo "" && echo "==> installing AWS CLI"
    # AWS CLI
-   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip
+   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip -o -q awscliv2.zip
    sudo ./aws/install
 }
 
