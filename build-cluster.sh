e3fbd90511c6287331ca9eaba7fe1756b1938322
254In61 <allanmaseghe4@gmail.com>
2024-02-29 14:58:08 +1000

    Thu 29 Feb 2024 14:58:08 AEST - Updates

---
+++ build-cluster.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+# Script that builds the cluster.
+
+jump_host(){
+   # 1: Prepare the ec2-jump-server.
+   scripts/ec2-env-prep.sh
+}
+
+set-vars(){
+    # Set env variables
+    echo "" && echo "Set environmental variables"
+    source ./env-vars
+}
+
+build-install-config-yaml(){
+    # Prepare install-config.yml
+    echo "" && echo "Build install-config.yml file"
+    ansible-playbook build-install-config-yaml.yml
+}
+
+install-cluster(){
+    # Install cluster
+    openshift-install create cluster --log-level debug
+}
+
+
+jump_host
+set-vars
+build-install-config-yaml
