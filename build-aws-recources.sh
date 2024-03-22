90768de7054080d5bbbd0ad247ec7770c8c3cfd9
254In61 <allanmaseghe4@gmail.com>
2024-03-22 14:39:41 +1000

    Fri 22 Mar 2024 14:39:41 AEST - Updates

---
+++ build-aws-recources.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+1_prep_ec2(){
+    scripts/ec2-env-prep.sh
+}
+
+2_ignition_files(){
+    ansible-playbook ignition.yml
+}
+
+3_bootstrap(){
+    ansible-playbook bootstrap.yml
+}
+
+4_master(){
+    ansible-playbook master.yml
+}
+
+5_initialize_bootstrap(){
+    openshift-install wait-for bootstrap-complete --dir=$HOME --log-level info
+}
+
+6_worker(){
+    ansible-playbook worker.yml
+}
+
+1_prep_ec2
+2_ignition_files
+3_bootstrap
+4_master
+# 5_initialize_bootstrap
+# 6_worker
+
+
