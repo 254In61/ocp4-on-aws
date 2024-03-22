3da3f91b79f7f47ef36088bc3b507da33db6adca
254In61 <allanmaseghe4@gmail.com>
2024-03-22 15:19:31 +1000

    Fri 22 Mar 2024 15:19:31 AEST - Updates

---
+++ build-infra.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+prep_ec2(){
+    echo "" && echo "==> Prepare the Bastion EC2 Linux environment"
+    scripts/ec2-env-prep.sh
+}
+
+ssh_agent(){
+    echo "" && echo "==> Start the ssh agent"
+    eval $(ssh-agent)
+}
+
+
+build_infra(){
+   
+    echo "" && echo "==> Build baseline infra"
+    ansible-playbook pre-infra.yml
+}
+
+
+prep_ec2
+ssh_agent
+ignition_files
+aws_services
+bootstrap
+master
+
+
