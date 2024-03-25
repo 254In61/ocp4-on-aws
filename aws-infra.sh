bd5d694d24a1ab27be9d8ccd89d673c7cec9cd57
254In61 <allanmaseghe4@gmail.com>
2024-03-25 18:40:20 +1000

    Mon 25 Mar 2024 18:40:20 AEST - Updates

--- aws-infra.sh
+++ aws-infra.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 echo "" && echo "===> git pull for any repo updates"
-rm -rf infra-build/terraform.tfvars
+# rm -rf infra-build/terraform.tfvars
 git pull
 
 # echo "" && echo "===> Change directory backwards to run ansible"
 # cd .. && ansible-playbook ignition-files.yml && cd infra-build
 
 
-echo "" && echo "===> Change directory back to run terraform"
-echo "" && echo "===> Run terraform apply"
-terraform apply
+echo "" && echo "===> Change directory to run terraform"
+cd infra-build/ && terraform apply
+cd ..
+
