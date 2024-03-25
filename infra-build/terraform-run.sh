49b7f28c3a449700420d21e37b957f9f0f084a98
254In61 <allanmaseghe4@gmail.com>
2024-03-25 17:18:59 +1000

    Mon 25 Mar 2024 17:18:59 AEST - Updates

--- infra-build/terraform-run.sh
+++ infra-build/terraform-run.sh
@@ -2,5 +2,10 @@
 echo "" && echo "===> git pull for any repo updates"
 git pull
 
+echo "" && echo "===> Change directory backwards to run ansible"
+cd .. && ansible-playbook ignition-files.yml
+
+
+echo "" && echo "===> Change directory back to run terraform"
 echo "" && echo "===> Run terraform apply"
-terraform apply
+cd infra-build && terraform apply
