51de6d27b019e912572afecdcc70da75ba0bcae3
254In61 <allanmaseghe4@gmail.com>
2024-03-25 16:31:59 +1000

    Mon 25 Mar 2024 16:31:59 AEST - Updates

--- infra-build/terraform-run.sh
+++ infra-build/terraform-run.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/bash
-echo "" && echo "===> git pull"
+echo "" && echo "===> git pull for any repo updates"
 git pull
 
-echo "" && echo "===> Extract hosted zone ID"
-hosted_zone_id=$(aws route53 list-hosted-zones-by-name --dns-name $BASE_DOMAIN --query 'HostedZones[0].Id' --output text | sed 's#/hostedzone/##')
-
-echo "" && echo "===> Extract infra name"
-infra_name=$(jq -r .infraID ~/metadata.json)
-
-echo "" && echo "===> Run terraform apply with variables included on CLI"
-terraform apply -var "infra_name=$infra_name" -var "cluster_name=$CLUSTER_NAME" -var "region=$REGION" -var "hosted_zone_id=$hosted_zone_id" -var "hosted_zone_name=$BASE_DOMAIN"
+echo "" && echo "===> Run terraform apply"
+terraform apply
