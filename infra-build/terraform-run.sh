2e0b5c83d36ce49290647093ca7ef383108856e0
254In61 <allanmaseghe4@gmail.com>
2024-03-25 10:42:07 +1000

    Mon 25 Mar 2024 10:42:07 AEST - Updates

--- infra-build/terraform-run.sh
+++ infra-build/terraform-run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
+echo "" && echo "===> git pull"
 git pull
 
+echo "" && echo "===> Extract infra name"
 infra_name=$(jq -r .infraID ~/metadata.json)
 
+echo "" && echo "===> Run terraform apply with variables included on CLI"
 terraform apply -var "infra_name=$infra_name" -var "cluster_name=$CLUSTER_NAME" -var "region=$REGION"
