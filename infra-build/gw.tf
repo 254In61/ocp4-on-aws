43526b735676e465438ca3c8d91f8f83861dbb99
254In61 <allanmaseghe4@gmail.com>
2024-03-25 11:09:50 +1000

    Mon 25 Mar 2024 11:09:50 AEST - Updates

--- infra-build/gw.tf
+++ infra-build/gw.tf
@@ -9,8 +9,10 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+/*
 resource "aws_internet_gateway_attachment" "attach-igw" {
   // Attach igw to vpc
   internet_gateway_id = aws_internet_gateway.igw.id
   vpc_id              = aws_vpc.cluster_vpc.id
 }
+*/
