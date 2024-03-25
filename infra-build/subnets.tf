ff707180b348e2b61a818052c6c1468655aa0fee
254In61 <allanmaseghe4@gmail.com>
2024-03-25 10:37:43 +1000

    Mon 25 Mar 2024 10:37:43 AEST - Updates

---
+++ infra-build/subnets.tf
@@ -0,0 +1,80 @@
+##############
+# SUBNETS
+##############
+
+// TO BE MADE DRY WITH TERRAGRUNT AT SOME POINT
+
+// availability zone a 
+
+resource "aws_subnet" "public-subnet-1" {
+ vpc_id               = aws_vpc.cluster_vpc.id
+ cidr_block           = "${var.public_subnet_1.cidr}"
+ availability_zone    = "${var.public_subnet_1.az}"
+ 
+ tags = {
+   "Name"                                    = "${var.infra_name}-public-${var.public_subnet_1.az}"
+   "kubernetes.io/cluster/${var.infra_name}" = "owned"
+ }
+}
+
+resource "aws_subnet" "private-subnet-1" {
+ vpc_id               = aws_vpc.cluster_vpc.id
+ cidr_block           = "${var.private_subnet_1.cidr}"
+ availability_zone    = "${var.private_subnet_1.az}"
+ 
+ tags = {
+   "Name"                                    = "${var.infra_name}-private-${var.private_subnet_1.az}"
+   "kubernetes.io/cluster/${var.infra_name}" = "owned"
+   "kubernetes.io/role/internal-elb"         = ""
+ }
+}
+
+// availability zone b
+
+resource "aws_subnet" "public-subnet-2" {
+ vpc_id               = aws_vpc.cluster_vpc.id
+ cidr_block           = "${var.public_subnet_2.cidr}"
+ availability_zone    = "${var.public_subnet_2.az}"
+ 
+ tags = {
+   "Name"                                    = "${var.infra_name}-public-${var.public_subnet_2.az}"
+   "kubernetes.io/cluster/${var.infra_name}" = "owned"
+ }
+}
+
+resource "aws_subnet" "private-subnet-1" {
+ vpc_id               = aws_vpc.cluster_vpc.id
+ cidr_block           = "${var.private_subnet_2.cidr}"
+ availability_zone    = "${var.private_subnet_2.az}"
+ 
+ tags = {
+   "Name"                                    = "${var.infra_name}-private-${var.private_subnet_2.az}"
+   "kubernetes.io/cluster/${var.infra_name}" = "owned"
+   "kubernetes.io/role/internal-elb"         = ""
+ }
+}
+
+// availability zone c
+
+resource "aws_subnet" "public-subnet-3" {
+ vpc_id               = aws_vpc.cluster_vpc.id
+ cidr_block           = "${var.public_subnet_3.cidr}"
+ availability_zone    = "${var.public_subnet_3.az}"
+ 
+ tags = {
+   "Name"                                    = "${var.infra_name}-public-${var.public_subnet_3.az}"
+   "kubernetes.io/cluster/${var.infra_name}" = "owned"
+ }
+}
+
+resource "aws_subnet" "private-subnet-3" {
+ vpc_id               = aws_vpc.cluster_vpc.id
+ cidr_block           = "${var.private_subnet_3.cidr}"
+ availability_zone    = "${var.private_subnet_3.az}"
+ 
+ tags = {
+   "Name"                                    = "${var.infra_name}-private-${var.private_subnet_3.az}"
+   "kubernetes.io/cluster/${var.infra_name}" = "owned"
+   "kubernetes.io/role/internal-elb"         = ""
+ }
+}
---
+++ infra-build/vpc.tf
@@ -0,0 +1,12 @@
+##############
+# VPC
+##############
+resource "aws_vpc" "cluster_vpc" {
+  cidr_block            = "${var.vpc_cidr}"
+  enable_dns_hostnames  = true
+  enable_dns_support    = true
+  tags                  = {
+     "kubernetes.io/cluster/${var.infra_name}" = "owned"
+     "Name"                                    = "${var.cluster_name}-vpc"
+  }
+}
