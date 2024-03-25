2f60d712e30929ee338f6d241838736819b4a48e
254In61 <allanmaseghe4@gmail.com>
2024-03-25 13:04:12 +1000

    Mon 25 Mar 2024 13:04:12 AEST - Updates

--- infra-build/vpc.tf
+++ infra-build/vpc.tf
@@ -5,9 +5,9 @@ resource "aws_vpc" "cluster_vpc" {
   cidr_block            = "${var.vpc_cidr}"
   enable_dns_hostnames  = true
   enable_dns_support    = true
-  
+
   tags                  = {
      "kubernetes.io/cluster/${var.infra_name}" = "owned"
-     "Name"                                    = "${var.cluster_name}-vpc"
+     "Name"                                    = "${var.infra_name}-vpc"
   }
 }
