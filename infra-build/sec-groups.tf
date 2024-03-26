e4abc411494c0b8e1d03e55ba275ecfb24ed885b
254In61 <allanmaseghe4@gmail.com>
2024-03-26 12:32:08 +1000

    Tue 26 Mar 2024 12:32:08 AEST - Updates

--- infra-build/sec-groups.tf
+++ infra-build/sec-groups.tf
@@ -9,10 +9,24 @@ resource "aws_security_group" "master-sg" {
   description = "Cluster Master Security Group"
   vpc_id = aws_vpc.cluster_vpc.id
 
-  ingress {
-    description      = "icmp traffic"
+  /*
+  By default, AWS creates an ALLOW ALL egress rule when creating a new Security Group 
+  inside of a VPC. When creating a new Security Group inside a VPC, Terraform will 
+  remove this default rule, and require you specifically re-create it if you desire that rule. 
+  We feel this leads to fewer surprises in terms of controlling your egress rules. 
+  */
+  egress {
     from_port        = 0
     to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "icmp traffic"
+    from_port        = -1
+    to_port          = -1
     protocol         = "icmp"
     cidr_blocks      = [aws_vpc.cluster_vpc.cidr_block]
   }
@@ -53,11 +67,26 @@ resource "aws_security_group" "worker-sg" {
   // name        = "allow_tls"
   description = "Cluster Worker Security Group"
   vpc_id = aws_vpc.cluster_vpc.id
+  
+  /*
+  By default, AWS creates an ALLOW ALL egress rule when creating a new Security Group 
+  inside of a VPC. When creating a new Security Group inside a VPC, Terraform will 
+  remove this default rule, and require you specifically re-create it if you desire that rule. 
+  We feel this leads to fewer surprises in terms of controlling your egress rules. 
+  */
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   ingress {
     description      = "icmp traffic"
-    from_port        = 0
-    to_port          = 0
+    from_port        = -1
+    to_port          = -1
     protocol         = "icmp"
     cidr_blocks      = [aws_vpc.cluster_vpc.cidr_block]
   }
