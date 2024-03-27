700fbc51bca873c31da2c8b620a2d244aad3a0c8
254In61 <allanmaseghe4@gmail.com>
2024-03-27 16:58:17 +1000

    Wed 27 Mar 2024 16:58:17 AEST - Updates

--- infra-build.sh
+++ infra-build.sh
@@ -1,23 +1,14 @@
 #!/usr/bin/bash
 
-# Keep this separate from constant restart!
-# Whenever restarted a new infra name is created and that means destroying of
-# terraform-build infra and rebuilding new ones!
-
-ssh_agent(){
-    echo "" && echo "==> Start the ssh agent"
-    eval $(ssh-agent)
-}
-
-build_ignition_files(){
+terraform_vars(){
     # One day I will remove Ansible and put Bash or Python script
-    echo "" && echo "==> Build ignition files"
+    echo "" && echo "==> Update terraform.tvars file using ansible"
     cd config-files/ && ansible-playbook terraform-vars.yml && cd ..
 }
 
 terraform_iac(){
-    echo "" && echo "===> Change directory to run terraform"
-    cd infra-build/ && terraform init && terraform apply && cd ..
+    echo "" && echo "===> Run terraform IAC"
+    cd infra-build/ && terraform init && terraform apply -auto-approve && cd ..
 }
 
 echo "" && echo "git pull for updates..."
