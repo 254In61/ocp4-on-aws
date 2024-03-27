48a89e30e8cf953d048eaeb60c8118bf1056233b
254In61 <allanmaseghe4@gmail.com>
2024-03-27 17:35:54 +1000

    Wed 27 Mar 2024 17:35:54 AEST - Updates

---
+++ infra.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+terraform_vars(){
+    # One day I will remove Ansible and put Bash or Python script
+    echo "" && echo "==> Update terraform.tvars file using ansible"
+    cd config-files/ && ansible-playbook terraform-vars.yml && cd ..
+}
+
+terraform_iac(){
+    echo "" && echo "===> Run terraform IAC"
+    cd aws-infra-build/ && terraform init && terraform apply -auto-approve && cd ..
+}
+
+echo "" && echo "git pull for updates..."
+git pull
+terraform_vars
+terraform_iac
