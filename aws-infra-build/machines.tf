b5ad3b8edd63baac62f0c7e9bb6701048c71a118
254In61 <allanmaseghe4@gmail.com>
2024-03-27 21:42:00 +1000

    Wed 27 Mar 2024 21:42:00 AEST - Updates

--- aws-infra-build/machines.tf
+++ aws-infra-build/machines.tf
@@ -2,6 +2,7 @@
 
 // 1. BOOTSTRAP INSTANCE
 
+*/
 resource "aws_network_interface" "bootstrap-ni" {
   subnet_id                   = aws_subnet.public-subnet-1.id
   // private_ips              = ["${var.bootstrap_priv_ip}"]
@@ -14,6 +15,7 @@ resource "aws_network_interface" "bootstrap-ni" {
    "kubernetes.io/cluster/${var.infra_name}" = "owned"
   }
 }
+*/
 
 resource "aws_instance" "bootstrap-machine" {
   ami                         = "${var.aws_rhos_ami}"
@@ -21,6 +23,8 @@ resource "aws_instance" "bootstrap-machine" {
   instance_type               = "${var.bootstrap_ec2_instance_type}"
   key_name                    = "${var.ec2_key_pair}"
   associate_public_ip_address = true
+  security_groups             = [aws_security_group.bootstrap-sg.id, aws_security_group.master-sg.id]
+  subnet_id                   = aws_subnet.public-subnet-1.id
   
   /*
   network_interface {
@@ -42,11 +46,13 @@ resource "aws_instance" "bootstrap-machine" {
   }
 }
 
+/*
 resource "aws_network_interface_attachment" "bootstrap-ni-attach" {
   instance_id          = aws_instance.bootstrap-machine.id
   network_interface_id = aws_network_interface.bootstrap-ni.id
   device_index         = 0
 }
+*/
 
 
 /*
